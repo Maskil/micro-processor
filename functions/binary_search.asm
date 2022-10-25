b56f8755adb54ccecd573edf8500737d2b32b9e4
Maskil <makigo613@gmail.com>
2022-10-25 17:15:58 +0900

    updated comments

--- functions/binary_search.asm
+++ functions/binary_search.asm
@@ -9,24 +9,6 @@ main:	la 	t0, target
 	li	a7, 5
 	ecall
 	sw 	a0, 0(t0)
-	mv	t0, a0
+	mv	t0, a0		#t0=target
+	# decide upper, lower, make length(15) a constant (put it in a non-temporary register)
 	
-	la	t1, numbers
-	li	t2, 15
-	mv	t3, t2
-	srli	t3, t3, 1
-	addi	t1, t1, t3
-	
-	# decide upper,lower values
-	
-loop:	lw	a0, 0(t1)
-	beq	t0, a0, fnd
-	blt	t0, a0, left
-	
-
-fnd:	li 	a7, 4		# syscall 4 (PrintString)
-	la	a0, mes1	# argument a0=mes1(found)
-	jal	end		# goto end
-nfnd:	li 	a7, 4		# syscall 4 (PrintString)
-	la	a0, mes2	# argument a0=mes2(found)
-end:	ecall	
